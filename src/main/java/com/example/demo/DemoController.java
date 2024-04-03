ae291cf8cf16ef546281eaf2b535abdedfd92685
Innam Park <pin954562@gmail.com>
2024-04-03 23:19:13 +0900

    add port

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,14 +1,16 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+  @Value("${server.port}")
+  private String serverPort;
 
   @GetMapping("/")
   public String getDemo() {
-    return "Hello World!";
+    return "Hello World! port:" + serverPort;
   }
-
 }
