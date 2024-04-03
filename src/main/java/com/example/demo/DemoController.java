5c8b29a3f3ea51444a180b21c470068dd846b29e
Innam Park <pin954562@gmail.com>
2024-04-03 21:16:59 +0900

    test

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,14 +1,17 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+  @Value("${server.port}")
+  private String serverPort;
 
   @GetMapping("/")
   public String getDemo() {
-    return "Hello World!4 ";
+    return "Hello World! port:" + serverPort;
   }
 
 }
